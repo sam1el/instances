000e8edd9be179f8be3eae64617aa7e6d228d4fd
Jeff Brimager <jbrimager@chef.io>
2020-06-04 17:14:23 -0500

    adding multi instances
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- main.tf
+++ main.tf
@@ -15,5 +15,16 @@ module "webserver" {
   key_name  = data.terraform_remote_state.network_details.outputs.vm_ssh_key
   subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
   vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id
-  tags = "student.2-vm1"
+  tags = "student.2-webserver-vm"
   }
+
+module "loadbalancer" {
+  source = "./modules/linux_node"
+  instance_count = "1"
+  ami = "ami-029fa1c105b8cc9e1"
+  instance_type = "t2.micro"
+  key_name = data.terraform_remote_state.network_details.outputs.vm_ssh_key
+  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+  vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id
+  tags = "student.2-loadbalancer-vm"
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "main_public_ip" {
   value = module.webserver.*.vm_public_ip
 }
+
+output "loadbalancer_vm_public_ip" {
+  value = module.loadbalancer.*.vm_public_ip
+}
