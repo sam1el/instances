9a1bf240837d34367315ff42a86fbd704632c376
Jeff Brimager <jbrimager@chef.io>
2020-06-04 10:44:24 -0500

    correcting backend
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- backend_state.tf
+++ backend_state.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "student.2-jeffe-bucket"
-    key    = "student.2-network-state"
+    key    = "student.2-instsance-state"
     region = "us-east-2"
   }
 }
-
